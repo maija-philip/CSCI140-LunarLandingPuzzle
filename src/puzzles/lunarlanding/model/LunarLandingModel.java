94aefea37f79c8fabf4ce0c2ac59d222c3f66034
Maija Philip <mep4741@rit.edu>
2021-12-01 12:47:03 -0500

    finished reload and load methods and set up and started model and ptui

--- src/puzzles/lunarlanding/model/LunarLandingModel.java
+++ src/puzzles/lunarlanding/model/LunarLandingModel.java
@@ -1,11 +1,24 @@
 package puzzles.lunarlanding.model;
 
+import util.Observer;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * DESCRIPTION
- * @author YOUR NAME HERE
+ * @author Maija Philip
  * November 2021
  */
 public class LunarLandingModel {
+
+    /**
+     * Those objects that are watching this object's every move
+     */
+    private List<Observer< LunarLandingModel, Object >> observers;
+    private String currentFilename;
 
     private LunarLandingConfig currentConfig;
 
@@ -20,4 +33,85 @@ public class LunarLandingModel {
      *   a cast of Config to LunarLandingConfig somewhere, since the solve
      *   method works with, and returns, objects of type Configuration.
      */
+
+    /**
+     * Construct a LunarLandingModel
+     */
+    public LunarLandingModel(String filename) {
+        this.observers = new LinkedList<>();
+
+        try {
+            this.currentConfig = new LunarLandingConfig(filename);
+        } catch (FileNotFoundException e) {
+            System.out.println("File \"" + filename + "\" not found");
+            return;
+        }
+
+        this.currentFilename = filename;
+        this.reset();
+    }
+
+    /**
+     * resets the board
+     */
+    public void reset() {
+        System.out.println("reset");
+
+    }
+
+    public void load(String filename) {
+        System.out.println("load");
+
+        try {
+            this.currentConfig = new LunarLandingConfig(filename);
+        } catch (FileNotFoundException e) {
+            System.out.println("File \"" + filename + "\" not found");
+            return;
+        }
+
+        this.currentFilename = filename;
+        this.reset();
+    }
+
+    public void reload() {
+        System.out.println("reload");
+        try {
+            currentConfig = new LunarLandingConfig(currentFilename);
+        } catch (FileNotFoundException e) {
+            System.out.println("File \"" + currentFilename + "\" not found");
+            return;
+        }
+        this.reset();
+    }
+
+    public void move(String move) {
+        System.out.println("move: " + move);
+    }
+
+    public void hint() {
+        System.out.println("hint");
+    }
+
+    public LunarLandingConfig getCurrentConfig() {
+        return currentConfig;
+    }
+
+    /**
+     * Add a new observer to the list for this model
+     * @param obs an object that wants an
+     *            {@link Observer#update(Object, Object)}
+     *            when something changes here
+     */
+    public void addObserver( Observer< LunarLandingModel, Object > obs ) {
+        this.observers.add( obs );
+    }
+
+    /**
+     * Announce to observers the model has changed;
+     */
+    private void announce( String arg ) {
+        for ( var obs : this.observers ) {
+            obs.update( this, arg );
+        }
+    }
 }
--- src/puzzles/lunarlanding/ptui/LunarLandingPTUI.java
+++ src/puzzles/lunarlanding/ptui/LunarLandingPTUI.java
@@ -1,13 +1,87 @@
 package puzzles.lunarlanding.ptui;
 
+import puzzles.lunarlanding.model.LunarLandingConfig;
+import puzzles.lunarlanding.model.LunarLandingModel;
+import puzzles.tipover.model.TipOverModel;
+import puzzles.tipover.ptui.TipOverPTUI;
+import util.Observer;
+
+import java.util.Scanner;
+
 /**
  * DESCRIPTION
- * @author YOUR NAME HERE
+ * @author Maija Philip
  * November 2021
  */
-public class LunarLandingPTUI {
+public class LunarLandingPTUI implements Observer< LunarLandingModel, Object > {
 
-    public static void main( String[] args ) {
-        System.err.println( "REPLACE THIS METHOD!" );
+    private LunarLandingModel model;
+    private int movesNum;
+
+    public LunarLandingPTUI(String filename){
+        this.model = new LunarLandingModel(filename);
+        initializeView();
+    }
+
+    private void run(){
+        Scanner in = new Scanner(System.in);
+        for( ; ; ){
+            System.out.println("game command: ");
+            String line = in.nextLine();
+            String[] words = line.split(("\\s+"));
+            if(words.length > 0){
+                if(words[0].startsWith("q")){
+                    break;
+                } else if (words[0].startsWith("l")){
+                    this.model.load(words[1]);
+                    displayBoard(movesNum);
+                } else if (words[0].startsWith("r")){
+                    this.model.reload();
+                    movesNum = 0;
+                    displayBoard(movesNum);
+                } else if (words[0].startsWith("m")){
+                    this.model.move(words[1]);
+                    this.movesNum+=1;
+                } else if (words[0].startsWith("hi")){
+                    this.model.hint();
+                } else if (words[0].startsWith("s")){
+                    displayBoard(movesNum);
+                } else {
+                    help();
+                }
+            }
+        }
+    }
+
+    public void initializeView(){
+        this.model.addObserver(this);
+        update(this.model,null);
+    }
+
+    public void displayBoard(int numMoves){
+        System.out.println("Move " + numMoves + ": ");
+        System.out.println(this.model.getCurrentConfig());
+    }
+
+    public void help(){
+        System.out.println("l(oad) f -- load new puzzle from file f");
+        System.out.println("r(eload) -- reload current puzzle");
+        System.out.println("m(ove) d -- move tipper in d direction");
+        System.out.println("            UP, DOWN, LEFT, RIGHT");
+        System.out.println("hi(nt)   -- receive hint");
+        System.out.println("s(how)   -- display the board");
+        System.out.println("he(lp)   -- show all commands");
+        System.out.println("q(uit)   -- quit the game");
+    }
+
+
+    @Override
+    public void update(LunarLandingModel lunarLandingModel, Object o) {
+        displayBoard(this.movesNum);
+    }
+
+    public static void main(String[] args ) {
+        LunarLandingPTUI ptui = new LunarLandingPTUI(args[0]);
+        ptui.run();
     }
 }
